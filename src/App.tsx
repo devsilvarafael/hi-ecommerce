da6c55f58c349a0f66605acff799f9000e48d8ed
Rafael Silva <devsilva.rafael@gmail.com>
2022-07-08 13:31:17 -0300

    config eslint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 function App() {
   return <p>Hello World</p>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// eslint-disable-next-line import/extensions
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
