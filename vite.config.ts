14f7c048bff5dee1d454af8cd6056773f167f20f
Rafael Silva <devsilva.rafael@gmail.com>
2022-07-10 19:56:02 -0300

    chore: adjust themes config and create @pages imports in tsconfig and viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
       "@public": path.resolve(__dirname, "./src/public"),
       "@theme": path.resolve(__dirname, "./src/theme"),
     },
